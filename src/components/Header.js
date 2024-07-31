29d7524ee36fc52b00c05d6e0de0b974045c8bb5
Sai Reddy <baroorsai3661@gmail.com>
2024-07-31 10:13:42 +0530

    added images

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,29 @@ import React from "react";
 
 const Header = () => {
   return (
-    <Flex bgImage={"/"} p={4} alignItems="center">
-      <Box>
-        <h1>SKYCHEX</h1>
-      </Box>
-      <Spacer />
-      <Button colorScheme="black" variant="outline" mr={4}>
-        Login
-      </Button>
-      <Button colorScheme="black">Sign Up</Button>
-    </Flex>
+    <Box
+      bg="white"
+      boxShadow="md"
+      borderRadius="md"
+      mb={8}
+      backgroundImage="url('/public/images/flight.jpg')" // Add the path to your background image here
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover"
+    >
+      <Flex p={4} alignItems="center">
+        <Box>
+          <h1>SKYCHEX</h1>
+        </Box>
+        <Spacer />
+        <Button colorScheme="black" variant="outline" mr={4}>
+          Sign Up
+        </Button>
+        <Button textColor="black" bgColor="white" colorScheme="blue">
+          Login
+        </Button>
+      </Flex>
+    </Box>
   );
 };
 
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,19 +1,28 @@
-import { Box, Button, Container, Flex, Input, Select } from "@chakra-ui/react";
+import { Box, Button, Flex, Input, Select } from "@chakra-ui/react";
 import React from "react";
 
 const SearchForm = () => {
   return (
-    <Container>
-      <Box bg="white" borderRadius="md" boxShadow="md" mt={4}>
-        <Flex borderRadius={50} backgroundColor={"lightgray"} mb={4}>
-          <Button flex="1" mr={1} colorScheme="blue">
+    <Box>
+      <Box position={"absolute"} mx="520px">
+        <Flex borderRadius={50} mb={4}>
+          <Button bgColor="blue.300" flex="1" mr={1} color="white">
             Return
           </Button>
           <Button flex="1" colorScheme="gray">
             One way
           </Button>
         </Flex>
-        <Flex>
+      </Box>
+      <Box
+        width="800px"
+        bg="white"
+        borderRadius="10px"
+        ml="220px"
+        boxShadow="2xl"
+        mt="100px"
+      >
+        <Flex position={"relative"} mx="30px" pt="80px">
           <Flex mb={4}>
             <Input placeholder="From" mr={2} />
             <Input placeholder="To" />
@@ -25,12 +34,12 @@ const SearchForm = () => {
           <Flex mb={4}>
             <Select placeholder="Passengers and class" />
           </Flex>
+          <Button bgColor="blue.300" colorScheme="blue" mx="20px" width="20%">
+            Search
+          </Button>
         </Flex>
-        <Button colorScheme="blue" width="100%">
-          Search
-        </Button>
       </Box>
-    </Container>
+    </Box>
   );
 };
 
